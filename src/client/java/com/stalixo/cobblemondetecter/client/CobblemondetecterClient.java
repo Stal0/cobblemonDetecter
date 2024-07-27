064186764202b1b48e16fea710c402d46fcf62cc
Lucas Gabriel <lucasgabrielsantos398@gmail.com>
2024-07-27 13:00:10 -0300

    "Init"

---
+++ src/client/java/com/stalixo/cobblemondetecter/client/CobblemondetecterClient.java
@@ -0,0 +1,10 @@
+package com.stalixo.cobblemondetecter.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CobblemondetecterClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/com/stalixo/cobblemondetecter/Cobblemondetecter.java
@@ -0,0 +1,10 @@
+package com.stalixo.cobblemondetecter;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Cobblemondetecter implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
